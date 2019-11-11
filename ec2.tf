7986e2e17b0da1d72ec9b968ca109c61395acbb8
Josh <josh.espinas@xero.com>
2019-11-11 16:09:50 +1300

    Adds a tag for identification on AWS console

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,8 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "MyResourceName" {
   ami           = "ami-08a74056dfd30c986"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloAlexFromTerraform"
+  }
 }
